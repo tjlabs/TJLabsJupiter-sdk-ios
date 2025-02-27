ebbf6d00dd56cf4ff6bb69c3edbfbf65a0b88487
Leo <dev@tjlabscorp.com>
2025-02-27 18:22:58 +0900

    feat: JupiterConstants
    constants for jupiter

---
+++ TJLabsJupiter/Model/JupiterConstants.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+
+class JupiterConstants {
+    static let SECONDS_TO_MILLIS = 1000
+    static let OUTPUT_INTEVAL: TimeInterval = 1/5 // seconds
+    static let OSR_INTERVAL: TimeInterval = 2 // seconds
+}
