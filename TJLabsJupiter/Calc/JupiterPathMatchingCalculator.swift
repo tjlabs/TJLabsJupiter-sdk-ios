1b863af1c20676a39d80edf44cf22cb1d81cf42e
Leo <dev@tjlabscorp.com>
2025-03-25 14:23:36 +0900

    feat & refact: JupiterPathMatchingCalculator
     - add getPathMatchingHeadings function
     - change function input in pathMatching
     - add new variables

--- TJLabsJupiter/Calc/JupiterPathMatchingCalculator.swift
+++ TJLabsJupiter/Calc/JupiterPathMatchingCalculator.swift
@@ -7,6 +7,8 @@ class JupiterPathMatchingCalculator {
     
     var pathPixelData = [String: PathPixelData]()
     var entranceMatchingData = [String: [[Double]]]()
+    var region: String = JupiterRegion.KOREA.rawValue
+    var sectorId: Int = 0
     
     init() { }
     
@@ -18,89 +20,163 @@ class JupiterPathMatchingCalculator {
         self.entranceMatchingData[key] = data
     }
     
-    func pathMatching(key: String, result: JupiterResult, headingRange: Double, isUseHeading: Bool, mode: UserMode, paddingValues: [Double]) -> xyhs {
-        
-        let x = result.x
-        let y = result.y
-        let heading = result.absolute_heading
-        let building = result.building_name
-        let level = TJLabsUtilFunctions.shared.removeLevelDirectionString(levelName: result.level_name)
-        
+    func pathMatching(region: String, sectorId: Int, building: String, level: String, x: Double, y: Double, heading: Double, headingRange: Double, isUseHeading: Bool, mode: UserMode, paddingValues: [Double]) -> xyhs {
         var xyhs = xyhs(x: x, y: y, heading: heading, scale: 1.0)
         var bestHeading = heading
         
-        guard let pathPixelData = self.pathPixelData[key] else { return xyhs }
+        guard !building.isEmpty, !level.isEmpty else { return xyhs }
+        
+        let levelName = TJLabsUtilFunctions.shared.removeLevelDirectionString(levelName: level)
+        let key = "\(sectorId)_\(building)_\(levelName)"
+        
+        let checkAvailablePathPixelResult = checkIsAvailablePathPixelData(key: key)
         guard let entranceMatchingArea = self.entranceMatchingData[key] else { return xyhs }
         
-        guard !building.isEmpty, !level.isEmpty,
-              let mainType = self.pathPixelData[key]?.roadType,
-              let mainRoad = self.pathPixelData[key]?.road,
-              let mainMagScale = self.pathPixelData[key]?.roadScale,
-              let mainHeading = self.pathPixelData[key]?.roadHeading else {
-            return xyhs
-        }
-        let pathMatchingArea = self.checkInEntranceMatchingArea(entranceMatchingArea: entranceMatchingArea, x: x, y: y)
-
-        var idshArray = [[Double]]()
-        var idshArrayWhenFail = [[Double]]()
-        
-        if !mainRoad.isEmpty {
-            let roadX = mainRoad[0]
-            let roadY = mainRoad[1]
-
-            var xMin = x - paddingValues[0]
-            var xMax = x + paddingValues[2]
-            var yMin = y - paddingValues[1]
-            var yMax = y + paddingValues[3]
+        if checkAvailablePathPixelResult.0 {
+            let pathPixelData = checkAvailablePathPixelResult.1
+            let mainType = pathPixelData.roadType
+            let mainRoad = pathPixelData.road
+            let mainMagScale = pathPixelData.roadScale
+            let mainHeading = pathPixelData.roadHeading
+            let pathMatchingArea = self.checkInEntranceMatchingArea(entranceMatchingArea: entranceMatchingArea, x: x, y: y)
+            var idshArray = [[Double]]()
+            var idshArrayWhenFail = [[Double]]()
             
-            if paddingValues[0] != 0 || paddingValues[1] != 0 || paddingValues[2] != 0 || paddingValues[3] != 0 {
-                if pathMatchingArea.0 {
-                    xMin = pathMatchingArea.1[0]
-                    yMin = pathMatchingArea.1[1]
-                    xMax = pathMatchingArea.1[2]
-                    yMax = pathMatchingArea.1[3]
-                }
-            }
-            for i in 0..<roadX.count {
-                let xPath = roadX[i]
-                let yPath = roadY[i]
-                let pathTypeLoaded = mainType[i]
+            if !mainRoad.isEmpty {
+                let roadX = mainRoad[0]
+                let roadY = mainRoad[1]
 
-                // Skip this path type if conditions aren't met
-                if mode == .MODE_VEHICLE && pathTypeLoaded == 0 { continue }
-
-                // Check if the path is within the bounding box
-                if xPath >= xMin && xPath <= xMax, yPath >= yMin && yPath <= yMax {
-                    let distance = sqrt(pow(x - xPath, 2) + pow(y - yPath, 2))
-                    let magScale = mainMagScale[i]
-                    var idsh: [Double] = [Double(i), distance, magScale, heading]
-                    idshArrayWhenFail.append(idsh)
-
-                    if isUseHeading {
-                        if let headingData = getHeadingDataArray(mainHeading[i]) {
-                            let (isValid, correctedHeading) = validateHeading(heading: heading, HEADING_RANGE: headingRange, headingData: headingData, x: xPath, y: yPath)
-                            if isValid {
-                                idsh[3] = correctedHeading
-                                idshArray.append(idsh)
-                            }
-                        }
-                    } else {
-                        idshArray.append(idsh)
+                var xMin = x - paddingValues[0]
+                var xMax = x + paddingValues[2]
+                var yMin = y - paddingValues[1]
+                var yMax = y + paddingValues[3]
+                
+                if paddingValues[0] != 0 || paddingValues[1] != 0 || paddingValues[2] != 0 || paddingValues[3] != 0 {
+                    if pathMatchingArea.0 {
+                        xMin = pathMatchingArea.1[0]
+                        yMin = pathMatchingArea.1[1]
+                        xMax = pathMatchingArea.1[2]
+                        yMax = pathMatchingArea.1[3]
                     }
                 }
-            }
+                for i in 0..<roadX.count {
+                    let xPath = roadX[i]
+                    let yPath = roadY[i]
+                    let pathTypeLoaded = mainType[i]
 
-            if !idshArray.isEmpty {
-                let updatedXyhs = processIdshArray(idshArray: idshArray, roadX: roadX, roadY: roadY, inputXyhs: &xyhs, bestHeading: &bestHeading, isUseHeading: isUseHeading)
-                xyhs = updatedXyhs
-            } else {
-                let updatedXyhs = processFailedIdshArray(idshArrayWhenFail: idshArrayWhenFail, mainHeading: mainHeading, roadX: roadX, roadY: roadY, inputXyhs: &xyhs, bestHeading: &bestHeading)
-                xyhs = updatedXyhs
+                    // Skip this path type if conditions aren't met
+                    if mode == .MODE_VEHICLE && pathTypeLoaded == 0 { continue }
+
+                    // Check if the path is within the bounding box
+                    if xPath >= xMin && xPath <= xMax, yPath >= yMin && yPath <= yMax {
+                        let distance = sqrt(pow(x - xPath, 2) + pow(y - yPath, 2))
+                        let magScale = mainMagScale[i]
+                        var idsh: [Double] = [Double(i), distance, magScale, heading]
+                        idshArrayWhenFail.append(idsh)
+
+                        if isUseHeading {
+                            if let headingData = getHeadingDataArray(mainHeading[i]) {
+                                let (isValid, correctedHeading) = validateHeading(heading: heading, headingRange: headingRange, headingData: headingData, x: xPath, y: yPath)
+                                if isValid {
+                                    idsh[3] = correctedHeading
+                                    idshArray.append(idsh)
+                                }
+                            }
+                        } else {
+                            idshArray.append(idsh)
+                        }
+                    }
+                }
+
+                if !idshArray.isEmpty {
+                    let updatedXyhs = processIdshArray(idshArray: idshArray, roadX: roadX, roadY: roadY, inputXyhs: &xyhs, bestHeading: &bestHeading, isUseHeading: isUseHeading)
+                    xyhs = updatedXyhs
+                } else {
+                    let updatedXyhs = processFailedIdshArray(idshArrayWhenFail: idshArrayWhenFail, mainHeading: mainHeading, roadX: roadX, roadY: roadY, inputXyhs: &xyhs, bestHeading: &bestHeading)
+                    xyhs = updatedXyhs
+                }
             }
         }
 
         xyhs.heading = TJLabsUtilFunctions.shared.compensateDegree(xyhs.heading)
         return xyhs
+    }
+    
+    func getPathMatchingHeadings(region: String, building: String, level: String, x: Double, y: Double, paddingValue: Double, mode: UserMode) -> [Double] {
+        var headings: [Double] = []
+        let levelCopy: String = TJLabsUtilFunctions.shared.removeLevelDirectionString(levelName: level)
+        
+        if (!(building.isEmpty) && !(level.isEmpty)) {
+            let key: String = "\(self.sectorId)_\(building)_\(levelCopy)"
+            let checkAvailablePathPixelResult = self.checkIsAvailablePathPixelData(key: key)
+            if checkAvailablePathPixelResult.0 {
+                let pathPixelData = checkAvailablePathPixelResult.1
+                let mainType = pathPixelData.roadType
+                let mainRoad = pathPixelData.road
+                let mainHeading = pathPixelData.roadHeading
+                
+                if (!mainRoad.isEmpty) {
+                    let roadX = mainRoad[0]
+                    let roadY = mainRoad[1]
+                    
+                    let xMin = x - paddingValue
+                    let xMax = x + paddingValue
+                    let yMin = y - paddingValue
+                    let yMax = y + paddingValue
+                    
+                    for i in 0..<roadX.count {
+                        let xPath = roadX[i]
+                        let yPath = roadY[i]
+                        
+                        let pathTypeLoaded = mainType[i]
+                        if (mode == .MODE_VEHICLE) {
+                            if (pathTypeLoaded == 0) {
+                                continue
+                            }
+                        }
+                        
+                        if (xPath >= xMin && xPath <= xMax) {
+                            if (yPath >= yMin && yPath <= yMax) {
+                                let headingArray = mainHeading[i]
+                                if (!headingArray.isEmpty) {
+                                    let headingData = headingArray.components(separatedBy: ",")
+                                    for j in 0..<headingData.count {
+                                        if (!headingData[j].isEmpty) {
+                                            let value = Double(headingData[j])!
+                                            if (!headings.contains(value)) {
+                                                headings.append(value)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return headings
+    }
+    
+    private func checkIsAvailablePathPixelData(key: String) -> (Bool, PathPixelData) {
+        let emptyPathPixelData = PathPixelData(roadType: [], nodeNumber: [], road: [[]], roadMinMax: [], roadScale: [], roadHeading: [])
+        guard let pathPixelData = self.pathPixelData[key] else {
+            return (false, emptyPathPixelData)
+        }
+        
+        let mainType = pathPixelData.roadType
+        if mainType.isEmpty { return (false, emptyPathPixelData) }
+        
+        let mainRoad = pathPixelData.road
+        if mainRoad.isEmpty { return (false, emptyPathPixelData) }
+        
+        let mainMagScale = pathPixelData.roadScale
+        if mainMagScale.isEmpty { return (false, emptyPathPixelData) }
+        
+        let mainHeading = pathPixelData.roadHeading
+        if mainHeading.isEmpty { return (false, emptyPathPixelData) }
+        
+        return (true, pathPixelData)
     }
     
     func checkInEntranceMatchingArea(entranceMatchingArea: [[Double]], x: Double, y: Double) -> (Bool, [Double]) {
@@ -130,14 +206,14 @@ class JupiterPathMatchingCalculator {
         return headingData.isEmpty ? nil : headingData
     }
 
-    private func validateHeading(heading: Double, HEADING_RANGE: Double, headingData: [Double], x: Double, y: Double) -> (Bool, Double) {
+    private func validateHeading(heading: Double, headingRange: Double, headingData: [Double], x: Double, y: Double) -> (Bool, Double) {
         var diffHeading = [Double]()
         for mapHeading in headingData {
             let adjustedHeading = adjustHeading(heading, mapHeading)
             diffHeading.append(abs(adjustedHeading))
         }
         if let minHeading = diffHeading.min() {
-            let valid = minHeading < HEADING_RANGE
+            let valid = minHeading < headingRange
             return (valid, headingData[diffHeading.firstIndex(of: minHeading)!])
         }
         return (false, heading)
