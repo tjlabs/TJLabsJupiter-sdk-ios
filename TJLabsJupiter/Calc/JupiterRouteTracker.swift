33d1112d7b23df0d193d569b69ff8b3cae049f1a
Leo <dev@tjlabscorp.com>
2025-03-26 13:11:11 +0900

    feat: JupiterRouteTrack
    add getRouteTrackLevel

--- TJLabsJupiter/Calc/JupiterRouteTracker.swift
+++ TJLabsJupiter/Calc/JupiterRouteTracker.swift
@@ -123,6 +123,7 @@ class JupiterRouteTracker {
                         result.x = wardCoord.x
                         result.y = wardCoord.y
                         result.absolute_heading = wardCoord.heading
+                        result.level_name = getRouteTrackEndLevel()
                         print("(CheckRouteTracking) stopRouteTracking : normalizedRSSI = \(normalizedRSSI) // thresholdRSSI = \(thresholdRSSI)")
                         return normalizedRSSI >= thresholdRSSI ? (true, result) : (false, result)
                     } else {
@@ -154,6 +155,17 @@ class JupiterRouteTracker {
             }
         }
         return false
+    }
+    
+    private func getRouteTrackEndLevel() -> String {
+        if let entranceRouteData = entranceRoute[currentEntranceKey] {
+            let entraneRouteLevel = entranceRouteData.routeLevel
+            if !entraneRouteLevel.isEmpty {
+                let levelName = entraneRouteLevel[entraneRouteLevel.count-1]
+                return levelName
+            }
+        }
+        return ""
     }
 
 }
