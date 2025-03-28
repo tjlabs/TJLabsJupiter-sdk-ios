c997719820efb2a65642523c09f81ae6e4bf4aa7
Leo <dev@tjlabscorp.com>
2025-03-28 17:31:18 +0900

    feat: JupiterSection
    Constants for section

---
+++ TJLabsJupiter/Data/Constant/JupiterSection.swift
@@ -0,0 +1,7 @@
+
+class JupiterSection {
+    static let SECTION_STRAIGHT_ANGLE: Double = 10
+    static let REQUIRED_SECTION_STRAIGHT_LENGTH: Double = 6
+    static let REQUIRED_SECTION_REQUEST_LENGTH: Double = 25
+    static let REQUIRED_SECTION_REQUEST_LENGTH_IN_DR: Double = 10
+}
