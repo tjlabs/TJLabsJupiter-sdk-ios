c109afe59e078984f4dbadab01490644fd44fe66
Leo <dev@tjlabscorp.com>
2025-04-09 13:47:26 +0900

    feat: JupiterSection
    add new variables

--- TJLabsJupiter/Data/Constant/JupiterSection.swift
+++ TJLabsJupiter/Data/Constant/JupiterSection.swift
@@ -4,4 +4,9 @@ class JupiterSection {
     static let REQUIRED_SECTION_STRAIGHT_LENGTH: Double = 6
     static let REQUIRED_SECTION_REQUEST_LENGTH: Double = 25
     static let REQUIRED_SECTION_REQUEST_LENGTH_IN_DR: Double = 10
+    
+    static let SAME_COORD_THRESHOLD: Int = 4
+    static let STRAIGHT_SAME_COORD_THRESHOLD: Int = 6
+    static let MAGNETIC_NORTH_THRESHOLD = 100
+    static let MAGNETIC_COMPENSATION_DEGREE = 180 // LG : 180 Tips : -20
 }
