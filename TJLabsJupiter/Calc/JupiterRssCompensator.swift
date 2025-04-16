b64b99c497055418430fcdea0405df65b767c841
Leo <dev@tjlabscorp.com>
2025-04-16 10:23:47 +0900

    refact: JupiterRssCompensator
    edit estimateNormalizationScale function:
    if (normalizationScale.0) 내  normalizationScale 업데이트 빠짐 + else 문 없음

--- TJLabsJupiter/Calc/JupiterRssCompensator.swift
+++ TJLabsJupiter/Calc/JupiterRssCompensator.swift
@@ -56,6 +56,9 @@ class JupiterRssCompensator {
                                 self.isScaleConverged = true
                             }
                             self.preNormalizationScale = smoothedScale
+                        } else {
+                            let smoothedScale: Double = smoothNormalizationScale(scale: self.preNormalizationScale)
+                            self.normalizationScale = smoothedScale
                         }
                     }
                 }
