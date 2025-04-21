c4f7c9b95b7616ba4627c14317aeddb251b716ed
Leo <dev@tjlabscorp.com>
2025-04-21 11:12:14 +0900

    feat: JupiterTime
    add new constants

--- TJLabsJupiter/Data/Constant/JupiterTime.swift
+++ TJLabsJupiter/Data/Constant/JupiterTime.swift
@@ -9,4 +9,8 @@ class JupiterTime {
     static let UVD_INTERVAL: TimeInterval = 1/40
     
     static let SAMPLE_HZ: Double = 40 // Hz
+    
+    static let MINIMUM_BUILDING_LEVEL_CHANGE_TIME: Double = 7000
+    static let TIME_INIT_THRESHOLD: Double = 25 * 1000 // seconds
+    static let TIME_INIT: Double = TIME_INIT_THRESHOLD + 1000
 }
